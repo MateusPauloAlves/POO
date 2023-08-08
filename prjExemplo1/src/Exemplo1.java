c5336b37f47f46d2000de702cf5668a0e4152d1e
Mateus <mateus.alves8@fatec.sp.gov.br>
2023-08-08 11:23:44 -0300

    First Commit

---
+++ prjExemplo1/src/Exemplo1.java
@@ -0,0 +1,9 @@
+/**
+ *
+ * @author 0030482211023
+ */
+public class Exemplo1 {
+    public static void main(String[] args) {
+        System.out.println("Linguagem Java");
+    }
+}
---
+++ prjExemplo2/src/Exemplo2.java
@@ -0,0 +1,24 @@
+/**
+ *
+ * @author 0030482211023
+ */
+public class Exemplo2 {
+    public static void main(String[] args) {
+        int Idade;
+        double Altura;
+        char Sexo;
+        String Nome;
+        
+        
+        Idade = 27;
+        Altura = 67.8;
+        Sexo = 'M';
+        Nome = "Carlos Silveira";
+        
+        
+        System.out.println("Nome: " + Nome);
+        System.out.println("Sexo: " + Sexo);
+        System.out.println("Idade: " + Idade);
+        System.out.println("Altura: " + Altura);
+    }
+}
